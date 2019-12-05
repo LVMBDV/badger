1842ed956181d8fa178b91d552113fa8bca076cd
Ata Kuyumcu <ata.kuyumcu@yandex.ru>
2019-12-06 02:26:59 +0300

    infer badge style names from template files

--- src/badger/helpers/style.cr
+++ src/badger/helpers/style.cr
@@ -1,5 +1,7 @@
 module Helpers::Style
   extend self
 
-  AVAILABLE_BADGE_STYLES = ["flat", "plastic", "for-the-badge"]
+  {% begin %}
+  AVAILABLE_BADGE_STYLES = {{ `ls -1 res/templates`.strip.split("\n").map { |fname| fname.gsub(/\..+$/, "") } }}
+  {% end %}
 end
